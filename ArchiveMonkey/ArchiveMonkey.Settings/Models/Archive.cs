ce9f355f10a9c41c03c4c3b540230def8be578d0
Matthias <department.it@sti-bearings.com>
2018-02-16 10:45:09 +0100

    Created basic models for settings

---
+++ ArchiveMonkey/ArchiveMonkey.Settings/Models/Archive.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace ArchiveMonkey.Settings.Models
+{
+    [DataContract]
+    public class Archive
+    {
+        [DataMember(IsRequired = true)]
+        public ArchiveUsage Usage { get; set; }
+
+        [DataMember(IsRequired = true)]
+        public string UncPath { get; set; }
+
+        [DataMember(IsRequired = true)]
+        public string DisplayName { get; set; }
+    }
+
+    public enum ArchiveUsage
+    {
+        Input = 1,
+        Output = 2
+    }
+}
---
+++ ArchiveMonkey/ArchiveMonkey.Settings/Models/ArchiveMonkeySettings.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace ArchiveMonkey.Settings.Models
+{
+    [DataContract]
+    public class ArchiveMonkeySettings
+    {
+        [DataMember(IsRequired = true)]
+        public string BasePath { get; set; }
+
+        [DataMember]
+        public IList<Archive> DavidArchives { get; set; }
+
+        [DataMember]
+        public IList<ArchivingAction> ArchivingActions { get; set; }
+    }    
+}
